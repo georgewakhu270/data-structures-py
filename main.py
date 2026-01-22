a3c5557a13b8ad306f2e0ca82282eddb1cef33d9
georgewakhu270 <georgewakhu270@gmail.com>
2026-01-22 14:13:56 +0300

    Graph examples

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from trees import BinarySearchTree
+
+from trees import BinarySearchTree, GenericTree
+from graphs import Graph
 
 bst = BinarySearchTree()
 
@@ -12,3 +14,28 @@ print(bst.root.value)
 bst.traversal(bst.root)
 
 bst.search('mariah carey')
+
+print("")
+
+gt = GenericTree("CEO")
+gt.insert("CEO", "VP of Sales")
+gt.insert("CEO", "VP of Engineering")
+gt.insert("VP of Engineering", "Tech Lead")
+gt.insert("VP of Engineering", "QA Manager")
+gt.insert("Tech Lead", "Software Engineer")
+
+gt.traversal(gt.root)
+
+g = Graph()
+nodes = ['A', 'B', 'C', 'D', 'E']
+for n in nodes: g.add_vertex(n)
+
+g.add_edge('A', 'B')
+g.add_edge('A', 'C')
+g.add_edge('B', 'D')
+g.add_edge('C', 'E')
+
+print("DFS Recursive starting from A:")
+g.dfs_recursive('A')
+print("\nBFS starting from A")
+g.bfs('A')
