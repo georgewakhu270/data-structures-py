4111c79904afce2e725d5276387fcfd5342f17c5
georgewakhu270 <georgewakhu270@gmail.com>
2026-01-22 11:58:35 +0300

    Added a GenericTreeNode class

--- trees.py
+++ trees.py
@@ -1,5 +1,12 @@
-class TreeNode:
+class BinaryTreeNode:
+  """This is for a binary tree where each parent node has at most 2 children"""
   def __init__(self,value):
     self.value = value
     self.left = left
     self.right = right
+
+class GenericTreeNode:
+  """This is for a generic tree where parents have at most more than 2 children"""
+  def __init__(self, value):
+    self.value = value
+    self.children = []
