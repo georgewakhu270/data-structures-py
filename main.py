53dab12683a8060937784bfac102c02a77f52b7b
georgewakhu270 <georgewakhu270@gmail.com>
2026-01-22 12:39:59 +0300

    Added a test item for the BinarySearchTree

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from trees import BinarySearchTree
 
+bst = BinarySearchTree()
+
+items = ['anne marie', 'katy perry','ariana grande', 'shania twain', 'jidenna', 'janealle monae', 'charlie puth', 'ed sheeran']
+
+for item in items:
+    bst.insert(item)
+    
+print(bst.root.value)
+
+bst.traversal(bst.root)
+
+bst.search('mariah carey')
